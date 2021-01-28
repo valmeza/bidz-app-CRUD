21aebfde7829e8232523565bfe8a689254861969
Jesus Rogelio Garza <jesusgarza2511@gmail.com>
2021-01-27 21:45:19 -0600

    Add the HTTP not found status throws in the ItemService ( orElseThrow() )

--- seeder.sql
+++ seeder.sql
@@ -1,4 +1,6 @@
+TRUNCATE TABLE items;
+
 INSERT INTO items (ID, NAME, DESCRIPTION, OWNER_ID, BIDDER_ID, PHOTO, BID_START_DATE_TIME, BID_END_DATE_TIME, BID_MINIMUM_AMOUNT, BID_INCREMENT, BID_AMOUNT)
-VALUES (1, 'car', 'A brand new red car', 'jess@gmail.com', 'Jess', 'car@car.com', '2016-06-22 19:10:25-07', '2021-06-22 19:10:25-07', 20.00, 50.00, 100.00),
-       (2, 'Tesla', 'A brand new tesla car', 'tom@gmail.com', 'Tommy', 'car@car.com', '2021-06-22 19:10:25-07', '2022-06-22 19:10:25-07', 40.00, 200.00, 50.00),
-       (3, 'CyberTruck', 'A brand truck', 'bibartan@gmail.com', 'Bibartan', 'car@car.com', '2024-06-22 19:10:25-07', '2025-06-22 19:10:25-07', 10.00, 10.00, 20.00);
+VALUES (1, 'car', 'A brand new red car', 'jess@gmail.com', 'Jess', 'https://images-na.ssl-images-amazon.com/images/I/71VEtPLgBxL._AC_SX569_.jpg', '2016-06-22 19:10:25-07', '2021-06-22 19:10:25-07', 20.00, 50.00, 100.00),
+       (2, 'Tesla', 'A brand new tesla car', 'tom@gmail.com', 'Tommy', 'https://cdn.shopify.com/s/files/1/0173/8204/7844/articles/Tesla-Model-S-Cheeta-Launch-Mode_1200x.jpg?v=1586427473', '2021-06-22 19:10:25-07', '2022-06-22 19:10:25-07', 40.00, 200.00, 50.00),
+       (3, 'CyberTruck', 'A brand new q', 'bibartan@gmail.com', 'Bibartan', 'https://i.pinimg.com/originals/cc/00/76/cc007607296f06d969c2dc32bf72b3a3.jpg', '2024-06-22 19:10:25-07', '2025-06-22 19:10:25-07', 10.00, 10.00, 20.00);
--- src/main/java/com/bidzapp/service/ItemService.java
+++ src/main/java/com/bidzapp/service/ItemService.java
@@ -3,7 +3,9 @@ package com.bidzapp.service;
 import com.bidzapp.model.Item;
 import com.bidzapp.repository.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,7 +23,11 @@ public class ItemService {
     public List<Item> getAllItems() { return itemRepository.findAll(); }
 
     public Item getItemById(long id) {
-        return itemRepository.findById(id).orElse(null);
+        return itemRepository
+                .findById(id)
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND, "Item not found in the database.")
+                );
     }
 
     public Item save(Item item) {
@@ -33,7 +39,12 @@ public class ItemService {
     }
 
     public void delete(long id) {
-        Item item = itemRepository.findById(id).orElse(null);
+        Item item = itemRepository
+                .findById(id)
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND, "Item not found in the database.")
+                );
+
         if(item != null) {
             itemRepository.delete(item);
         }
